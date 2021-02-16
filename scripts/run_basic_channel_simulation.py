d3a84545e89324e5b9565d9896f0b86ff731f420
Kyriakos <bekoulis@gmail.com>
2021-02-16 16:37:09 +0200

    starting reproducing exact experiments

--- scripts/run_basic_channel_simulation.py
+++ scripts/run_basic_channel_simulation.py
@@ -7,21 +7,22 @@ from core.surfaces import RIS
 from core.experiment_setups import PositionGrid
 from core.channels import Channel, GaussianFadeLink
 from core.exhaustive_phase_search import find_RIS_configuration_that_maximizes_SNR
+import core.globals as globals
 
 if __name__ == '__main__':
 
-    ris_elements     = 6
-    groups_of        = 2
-    number_surfaces  = 2
-    element_size     = [0.1, 0.1]
-    in_group_dist    = [0.1, 0.1]
-    out_group_dist   = [0.2, 0.2]
+    ris_elements     = globals.total_elements
+    groups_of        = globals.dependent_elements
+    number_surfaces  = globals.total_surfaces
+    element_size     = [globals.wavelength/2, globals.wavelength/2]
+    in_group_dist    = [globals.wavelength, globals.wavelength]
+    out_group_dist   = [globals.wavelength, globals.wavelength]
     phase_space      = ('discrete', {'values': [np.exp(1j*0), np.exp(1j*np.pi)]})
 
     ris_coords       = np.array([[0, 0, 0],
                                  [1, 1, 1]])
-    tx_coords        = np.array([[2, 2, 2]])
-    rx_coords        = np.array([[3, 3, 3]])
+    tx_coords        = globals.TX_location
+    rx_coords        = globals.RX_location
 
     r1 = RIS(ris_coords[0, :], (ris_elements, 1), (groups_of, 1), element_size, in_group_dist, out_group_dist, phase_space)
     r2 = RIS(ris_coords[1, :], (ris_elements, 1), (groups_of, 1), element_size, in_group_dist, out_group_dist, phase_space)
@@ -31,9 +32,9 @@ if __name__ == '__main__':
 
     #pg = PositionGrid(ris_coords, tx_coords, rx_coords)
 
-    mult_factor = 1
+    mult_factor = globals.mult_fact
     link_info = (GaussianFadeLink, {'mult_factor': mult_factor})
-    noise_power = 1
+    noise_power = globals.noisePower
 
     ch = Channel(tx_coords, rx_coords, link_info, link_info, link_info, noise_power)
 
